434e0d32fb747aac84ae7249ff250ba3628f8518
Geo <edgeorge.archbold@gmail.com>
2024-05-17 22:02:22 -0400

    feat: adds links to the icon and edits the index title. feat: creates a custom gradient transition for the submit button. info: further develops the logic for email validation using callback functions. refactor: adds import statements to the main stylesheet to clean up index.html. feat: hides a gradient behind the submit button and transitions the button's background to transperent to mimic a gradient transition. refactor: adds custom icons the bullet points. refactor: changes background color to an off white.

--- newsletter/newsletter-sign-up-with-success-message-main/script.js
+++ newsletter/newsletter-sign-up-with-success-message-main/script.js
@@ -1,35 +1,54 @@
 import { Modal } from "./Modal.js"
 document.addEventListener('DOMContentLoaded', (event) => {
 
+  
+  
   // Get dom elements for the email field and submission button
 
-  const submitButton = document.getElementById('button-submit') 
+  const submitButton = document.querySelector('.button-submit') 
 
   const emailField = document.getElementById('field-email')
 
   // When submit button is clicked, check the validity of the email
 
-  submitButton.addEventListener('click', () => {
+  function checkEmailValidity () {
 
-    console.log('Working')
-
-    const newModal = new Modal()
-
-    newModal.confirm()
-
-  })
-
-  emailField.addEventListener('change', () => {
-
-    const prompt = new Modal
+    const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/ // RegEx for email validation
 
     const emailValue = emailField.value 
 
-    const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/ // RegEx for email validation
-        
-    let validEmailAddress = emailRegex.test(emailValue)
+    return emailRegex.test(emailValue)
 
+  }
+
+  function emailValidationResponse(checkEmailValidity) {
+
+    if (!checkEmailValidity()) {
+
+      const validationPrompt = document.querySelector('.email-validation-prompt')
+
+      validationPrompt.innerText = "Valid email required"
+
+      return false
+
+    } 
     
+    return true
+
+  }
+
+  submitButton.addEventListener('click', (event) => {
+
+    event.preventDefault()
+    
+    const subscribedModal = new Modal()
+
+    subscribedModal.confirm()
+
+
   })
+  
+
+  
 
 }) 
