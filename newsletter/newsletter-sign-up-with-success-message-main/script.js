658f596185d8a391360496e5d53e2777b555b6fd
Geo <edgeorge.archbold@gmail.com>
2024-05-23 00:16:48 -0400

    modal styling bug 1.1
    refactor: changes the style import in modal.css to include a relative path.
    refactor: add the emailField value directly into the message.
    refactor: changes the imgAddress to include a relative path.

--- newsletter/newsletter-sign-up-with-success-message-main/script.js
+++ newsletter/newsletter-sign-up-with-success-message-main/script.js
@@ -62,9 +62,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
       removeInvalidEmailReponse()
 
       const title = "Thanks for subscribing!"
-      const email = emailField.value
-      const imgAddress = "/newsletter-sign-up-with-success-message-main/assets/images/icon-list.svg"
-      const message = `A confirmation email has been sent to ${email}. Please open it and click the button inside to confirm your subscription.`
+      const imgAddress = "./assets/images/icon-list.svg"
+      const message = `A confirmation email has been sent to ${emailField.value}. Please open it and click the button inside to confirm your subscription.`
       const btnMessage = "Dismiss message"
 
       const modal = new Modal(message, title, btnMessage, null, imgAddress);
