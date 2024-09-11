360b6521a2d6ad5deb8ac8a4d9a486a4010bc949
Geo <edgeorge.archbold@gmail.com>
2024-09-10 23:03:25 -0400

    contact form feat: implements emailJS to add functionality to the contact form. bug: fixes incorrect path.

--- src/logic/form-validation.js
+++ src/logic/form-validation.js
@@ -1,34 +1,69 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    const fieldInputs = document.querySelector(".contact-me-form").querySelectorAll(".field");
-    const sendMessageButton = document.querySelector("#contact-submit-button");
+    const fieldInputs = document.querySelector(".contact-me-form").querySelectorAll(".field")
+
+    const sendMessageButton = document.querySelector("#contact-submit-button")
 
     sendMessageButton.addEventListener("click", (event) => {
 
-        event.preventDefault();
+        event.preventDefault()
 
-        validateInputFields();
+        if (validInputFields()) {
+
+            const nameEl = document.getElementById('name')
+
+            const emailEl = document.getElementById('email')
+
+            const messageEl = document.getElementById('message')
+
+            emailjs.init('ef3-ZRdxfhqemg5QM')
+
+            emailjs.send('service_7bx4lzw', 'template_sdvcriy', {
+
+                from_name: nameEl.value,
+                from_email: emailEl.value,
+                message: messageEl.value
+
+            }).then( 
+                    
+                    response => { alert("Message sent successfully!") }, 
+                
+                    error => {
+
+                        alert("Failed to send message.")
+
+                    }
+                
+                )
+
+        }
         
-    });
+    })
 
-    function validateInputFields () {
+    function validInputFields () {
 
-        fieldInputs.forEach((e) => {
+        let validForm = true
 
-            const eTag = e.querySelector("input, textarea");
+        fieldInputs.forEach( e => {
+
+            const eTag = e.querySelector("input, textarea")
 
             if (eTag !== null && !eTag.value) {
 
-                e.querySelector("p").textContent = "This field is required.";
+                e.querySelector("p").textContent = "This field is required."
+
+                validForm = false
 
             } else {
 
-                e.querySelector("p").textContent = "";
+                if (e.querySelector("p")) e.querySelector("p").textContent = ""
 
             }
 
-        });
+        })
+
+        return validForm
 
     }
 
-});
+})
